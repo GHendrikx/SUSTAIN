c2617ff006c32fb5f7707de0bcda9ddfc0136619
Geoffrey86 <geoffreyhendrikx@hotmail.com>
2020-03-03 11:59:47 +0100

    prototype
    Push new prototype version

--- Context2/Assets/Scripts/DateTimer.cs
+++ Context2/Assets/Scripts/DateTimer.cs
@@ -1,26 +1,43 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using System;
 
 public class DateTimer : MonoBehaviour
 {
     [SerializeField]
-    private Text dateText;
-    
-    public Date date;
+    private TextMeshProUGUI month;
+    [SerializeField]
+    private TextMeshProUGUI year;
 
+    public Date date;
+    public void Start()
+    {
+        month.text = string.Format("{0}",(Months)date.currentMonth);
+        year.text = date.currentYear.ToString();
+    }
     public void NextDate()
     {
         date.currentMonth++;
-        if (date.currentMonth > 12)
+        if (date.currentMonth == 12)
         {
             date.currentYear++;
-            date.currentMonth = 1;
+            date.currentMonth = 0;
         }
 
-        dateText.text = string.Format("{0}/{1}",date.currentMonth,date.currentYear); 
+        month.text = string.Format("{0}", (Months)date.currentMonth);
+        year.text = string.Format("{0}", date.currentYear);
 
+        CheckEndDate();
     }
 
+    private void CheckEndDate()
+    {
+        if(date.currentMonth == date.endMonth && date.currentYear == date.endYear)
+        {
+            //GAME FUCKING OVER
+        }
+    }
 }
 [System.Serializable]
 public class Date
@@ -40,4 +57,20 @@ public class Date
         currentMonth = beginMonth;
     }
 
+}
+
+public enum Months
+{
+    January,
+    February,
+    March,
+    April,
+    May,
+    June,
+    Juli,
+    August,
+    September,
+    October,
+    November,
+    December
 }
