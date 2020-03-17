d7f0ca517aba41d9983ce2769e1a205181e5939c
erik_wiersma <erikwiersma97@gmail.com>
2020-03-17 11:13:59 +0100

    tutorial

---
+++ Context2/Assets/NameInput.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class NameInput : MonoBehaviour
+{
+    public string PlayerName;
+    public GameObject inputeField;
+    public GameObject storename;
+    public TextMeshProUGUI textDisplay;
+
+    public void StoreName()
+    {
+        PlayerName = inputeField.GetComponent<Text>().text;
+        //textDisplay.GetComponent<TextMeshProUGUI>().text = "well met " + PlayerName +  " \n That is an interesting name. \n" + PlayerName + " you will be tested by running the 3th SDG good health and well-being, \n if your programme runs smoothly you will be assigned to run additional SDG’s.";
+    }
+}
