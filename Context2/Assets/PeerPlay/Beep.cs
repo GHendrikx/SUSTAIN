bb3d11f834fef16d527261e6fd849fa870c91c9d
marnixlicht <mlt350@gmail.com>
2020-03-20 16:22:49 +0100

    Peerplay heartbeat prefab

---
+++ Context2/Assets/PeerPlay/Beep.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beep : MonoBehaviour
+{
+    public AudioPeer audioPeer;
+    [Range(0, 7)] private int audioBand = 7;
+
+    public bool Test;
+
+    private void Start()
+    {
+        StartCoroutine(MuteMic());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (audioPeer._audioBand[audioBand] > 0.01f)
+        {
+            Test = true;
+        }
+        else
+            Test = false;
+    }
+
+    private IEnumerator MuteMic()
+    {
+        yield return new WaitForSeconds(0.01f);
+        audioPeer._useMicrophone = true;
+    }
+}
