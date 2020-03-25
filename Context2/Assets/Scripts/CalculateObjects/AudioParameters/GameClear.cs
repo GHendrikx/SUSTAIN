dfa962a0b6859fb21cbee8531ad020453bebcc3a
marnixlicht <mlt350@gmail.com>
2020-03-25 16:41:26 +0100

    Merge branch 'master' of https://github.com/GHendrikx/SUSTAIN
    # Conflicts:
    #	Context2/Assets/Scripts/Interfaces/AIInterface.cs

---
+++ Context2/Assets/Scripts/CalculateObjects/AudioParameters/GameClear.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Context
+{
+    public class GameClear : MonoBehaviour
+    {
+        private int currentGameClear;
+        public int CurrentGameClear
+        {
+            get
+            {
+                return currentGameClear;
+            }
+            set
+            {
+                currentGameClear = value;
+            }
+        }
+
+        public void SetGameClear()
+        {
+            currentGameClear = 1;
+            AudioManager.Instance.SetParameters(-999, currentGameClear, -999, -999, GameManager.Instance.StudioParameterBGM);
+        }
+    }
+}
---
+++ Context2/Assets/Scripts/CalculateObjects/AudioParameters/PerformanceData.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Context
+{
+    public class PerformanceData : MonoBehaviour
+    {
+        private Data[] data;
+        [SerializeField]
+        private IOManager ioManager;
+
+        private float currentPerformance;
+        public float CurrentPerformance
+        {
+            get
+            {
+                return currentPerformance;
+            }
+            set
+            {
+                currentPerformance = value;
+            }
+        }
+
+        [SerializeField]
+        private AI ai;
+
+        // Start is called before the first frame update
+        void Start() =>
+            data = ioManager.data.Data;
+
+        public void CalculatePerformance()
+        {
+            currentPerformance = ai.AIFitnessScore.fillAmount * 10;
+            Debug.Log(ai.AIFitnessScore.fillAmount);
+            AudioManager.Instance.SetParameters(currentPerformance, -999, -999, -999, GameManager.Instance.StudioParameterBGM);
+        }
+
+    }
+}
